02c223782e807fe0d1ac49737c2a45846016b9b5
Srinidhi264 <your-email@example.com>
2025-09-26 12:15:33 +0530

    Update build artifacts with latest changes

---
+++ .vite/deps/react-dom_client.js
@@ -0,0 +1,8 @@
+import {
+  require_client
+} from "./chunk-JQRIRPDN.js";
+import "./chunk-BKJ7VDAQ.js";
+import "./chunk-32E4H3EV.js";
+import "./chunk-G3PMV62Z.js";
+export default require_client();
+//# sourceMappingURL=react-dom_client.js.map
